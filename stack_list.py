8c993a85740c25ab2b389b297dd22647424f18a0
Grant Gasser <glgasser@gmail.com>
2019-07-24 15:01:20 -0700

    Add stack_list file.

---
+++ stack_list.py
@@ -0,0 +1,10 @@
+## Stack implemented as Linked List class
+## Author: Grant Gasser
+## 7/24/2019
+
+# Inspired by Udacity Data Structures and Algorithms course
+
+from list import List
+
+class Stack(List):
+    pass
